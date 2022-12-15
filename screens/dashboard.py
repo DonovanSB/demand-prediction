cf6f8f2823428a58471ca4831810d32a14e9168b
DonovanSB <dvnstevenb@gmail.com>
2022-12-15 11:30:04 -0500

    fix negative values

--- screens/dashboard.py
+++ screens/dashboard.py
@@ -16,35 +16,40 @@ service = DataService()
 
 def on_click():
     global container
-    data = service.df
-    # data[data < 0] = 0
-    df1 = data.reset_index()["TkW"]
+    if service.df is not None:
+        data = service.df
+        data[data < 0] = 0
+        df1 = data.reset_index()["TkW"]
 
-    batch_size = 100
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+        batch_size = 100
+        device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    scalar = MinMaxScaler(feature_range=(0, 1))
-    df1 = scalar.fit_transform(np.array(df1).reshape(-1, 1))
+        scalar = MinMaxScaler(feature_range=(0, 1))
+        df1 = scalar.fit_transform(np.array(df1).reshape(-1, 1))
 
-    seq_len = 100
-    test_dataset = StockDataset(df1, seq_len)
-    data_loader = DataLoader(test_dataset, batch_size, drop_last=True)
+        seq_len = 100
+        test_dataset = StockDataset(df1, seq_len)
+        data_loader = DataLoader(test_dataset, batch_size, drop_last=True)
 
-    input_dim = 1
-    hidden_size = 10
-    num_layers = 2
+        input_dim = 1
+        hidden_size = 10
+        num_layers = 2
 
-    model = LstmModel(input_dim, hidden_size, num_layers).to(device)
-    parent = path.join(Path(__file__).parent.resolve(), pardir)
-    root = path.abspath(parent)
-    model.load_state_dict(torch.load(path.join(root, "models/demand_prediction.pt"), map_location=device))
-    model.to(device)
+        model = LstmModel(input_dim, hidden_size, num_layers).to(device)
+        parent = path.join(Path(__file__).parent.resolve(), pardir)
+        root = path.abspath(parent)
+        model.load_state_dict(
+            torch.load(
+                path.join(root, "models/demand_prediction.pt"), map_location=device
+            )
+        )
+        model.to(device)
 
-    metric, pred_arr, y_arr = calculate_metrics(model, scalar, data_loader)
-    with container:
-        st.title("Predicciones")
-        st.write("MSE", metric)
-        st.line_chart(pd.DataFrame({"prediction": pred_arr, "real": y_arr}))
+        metric, pred_arr, y_arr = calculate_metrics(model, scalar, data_loader)
+        with container:
+            st.title("Predicciones")
+            st.write("MSE", metric)
+            st.line_chart(pd.DataFrame({"prediction": pred_arr, "real": y_arr}))
 
 
 def dashboard():
--- screens/data_upload.py
+++ screens/data_upload.py
@@ -17,5 +17,5 @@ def data_upload():
 
         uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
-            df = pd.read_excel(uploaded_file)
+            df = pd.read_excel(uploaded_file, index_col='date')
             service.data_frame = df
