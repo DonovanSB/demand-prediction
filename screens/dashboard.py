dfb8632a0cd1aa15edec88255b1630b76f40792f
DonovanSB <dvnstevenb@gmail.com>
2022-12-14 23:34:04 -0500

    update torch

--- screens/dashboard.py
+++ screens/dashboard.py
@@ -1,10 +1,27 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+from torch.utils.data import DataLoader
+from sklearn.preprocessing import MinMaxScaler
 
 from services.data_service import DataService
 
 
 service = DataService()
+
+
+def on_click():
+    data = service.df
+    # data[data < 0] = 0
+    df1 = data.reset_index()["TkW"]
+    st.line_chart(df1)
+
+    scalar = MinMaxScaler(feature_range=(0, 1))
+    df1 = scalar.fit_transform(np.array(df1).reshape(-1, 1))
+
+    batch_size = 100
+    data_loader = DataLoader(df1, batch_size, drop_last=True)
+    st.write(len(data_loader))
 
 
 def dashboard():
@@ -16,5 +33,6 @@ def dashboard():
         if service.df is not None:
             st.write("Datos cargados")
             st.line_chart(service.df.TkW)
+            st.button("Realizar predicción", on_click=on_click)
         else:
             st.write("Por favor carga los datos")
