74174a57f489a51de16b52fc9843a740419857fb
DonovanSB <dvnstevenb@gmail.com>
2022-12-14 23:45:50 -0500

    define model

--- screens/dashboard.py
+++ screens/dashboard.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import torch
 from torch.utils.data import DataLoader
 from sklearn.preprocessing import MinMaxScaler
 
 from services.data_service import DataService
+from services.mode_service import LstmModel
 
 
 service = DataService()
@@ -14,14 +16,19 @@ def on_click():
     data = service.df
     # data[data < 0] = 0
     df1 = data.reset_index()["TkW"]
-    st.line_chart(df1)
 
     scalar = MinMaxScaler(feature_range=(0, 1))
     df1 = scalar.fit_transform(np.array(df1).reshape(-1, 1))
 
     batch_size = 100
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     data_loader = DataLoader(df1, batch_size, drop_last=True)
-    st.write(len(data_loader))
+
+    input_dim = 1
+    hidden_size = 512
+    num_layers = 2
+
+    model = LstmModel(input_dim, hidden_size, num_layers).to(device)
 
 
 def dashboard():
--- services/mode_service.py
+++ services/mode_service.py
@@ -1,5 +1,30 @@
+import torch
+import torch.nn as nn
+
+batch_size = 100
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-class Model:
-  def __init__(self) -> None:
-    pass
+class LstmModel(nn.Module):
+    def __init__(self, input_dim, hidden_size, num_layers, drop_prob=0.3):
+        super(LstmModel, self).__init__()
+        self.num_layers = num_layers
+        self.input_size = input_dim
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(
+            input_size=input_dim, hidden_size=hidden_size, num_layers=num_layers
+        )
+        self.dropout = nn.Dropout(drop_prob)
+        self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, x, hn, cn):
+        out, (hn, cn) = self.lstm(x, (hn, cn))
+        out = self.dropout(out)
+
+        final_out = self.fc(out[-1])
+        return final_out, hn, cn
+
+    def init(self):
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
+        return h0, c0
